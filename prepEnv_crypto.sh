d2afdfa892e48afffe7397689c5d9a928b5696ef
Samuel Bruce <sbruce1@firstderivatives.com>
2022-08-25 10:18:55 +1000

    Modifying startup script

--- prepEnv_crypto.sh
+++ prepEnv_crypto.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ./env_crypto
-
 eval "echo \"$(cat cfg/assembly_sample.yaml)\"" > cfg/resolved_assembly_sample.yaml
 #eval "echo \"$(cat SG/cfg/assembly_sample.yaml)\"" > SG/cfg/resolved_assembly_sample.yaml
 #eval "echo \"$(cat DA/cfg/assembly_sample.yaml)\"" > DA/cfg/resolved_assembly_sample.yaml
